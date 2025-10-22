dc9003cd286088b13198d15c80f389a7717a0e45
Ahmed Adel Hassan <aa3452000@gmail.com>
2025-10-22 04:40:54 +0300

    .

---
+++ Core/Store.G02.Domain/Contracts/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Store.G02.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Domain.Contracts
+{
+    public interface IGenericRepository<TKey , TEntity > where TEntity : BaseEntity<TKey>   
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync(bool ChangeTracker = false);
+        Task<TEntity?> GetAsync(TKey key);
+        Task AddAsync(TEntity entity);
+        void Update (TEntity entity);
+        void Delete (TEntity entity);
+    }
+}
